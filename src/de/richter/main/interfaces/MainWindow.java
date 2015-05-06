9584837c0e764108ac17192f095dd5b3a1ced9e6
lukas22497 <lukas_mueller@outlook.de>
2015-05-06 12:41:41 +0200

    Doppeltes initialize im Main-Window entfernt.
    Code nicht in Update-Methode ausgelagert.
    
    Aktueller Programmstand entspricht dieser Version.
    
    Ausgelagerter Code liegt als ZIP vor.

--- src/de/richter/main/interfaces/MainWindow.java
+++ src/de/richter/main/interfaces/MainWindow.java
@@ -67,7 +67,6 @@ public class MainWindow {
 				checkinWindow.getFrmEinchecken().addWindowListener(wl);
 				checkinWindow.getBtnClose().addActionListener(wl);
 				checkinWindow.getFrmEinchecken().setLocationRelativeTo(null);
-				initialize();
 				checkinWindow.getFrmEinchecken().setVisible(true);
 			}
 		});
